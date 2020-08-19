72a78826a4af53ea5a067573278579f2f8580fc8
BowedKinkajou50 <69907388+BowedKinkajou50@users.noreply.github.com>
2020-08-19 09:36:16 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Even Pham");
+		System.out.println("Hello, World");
+		System.out.println("Printing Hello World");
+		System.out.println("8/19/2020");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+		}
+	}
